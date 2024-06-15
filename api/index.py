7fbbf8437fc45d493270f7d14b1e55166b5762b7
James Cuadra <jjlcuadradev@gmail.com>
2024-06-15 14:18:11 +0800

    Added main video download functionality

--- api/index.py
+++ api/index.py
@@ -1,22 +1,122 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, current_app, send_from_directory
 from flask_cors import CORS
+from pytube import YouTube
+from collections import OrderedDict
+import shutil
+import os
+import re
+
 
 # App instance
 app = Flask(__name__)
 CORS(app)
 
-# Setting up endpoint
-@app.route("/api/home", methods=["POST"])
-def return_home():
+
+VIDEO_FOLDER = "/tmp/downloaded_yt_video"
+
+# Check if the upload folder exists in /tmp and create it if it doesn't
+if not os.path.exists(VIDEO_FOLDER):
+    os.makedirs(VIDEO_FOLDER)
+else:
+    print(f"{VIDEO_FOLDER} folder already exists.")
+
+
+# Initialize the application context
+with app.app_context():
+    current_app.available_resolutions = OrderedDict()
+
+
+def delete_all_files():
+    # Delete all old uploaded images
+    for filename in os.listdir(VIDEO_FOLDER):
+        file_path = os.path.join(VIDEO_FOLDER, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
+
+# Converting Stream to List of Dictionaries
+def convert_to_dict_list(stream_list):
+    dict_list = []
+    for stream in stream_list:
+        # Extracting the contents between the angle brackets
+        stream_str = str(stream)
+        attributes = re.findall(r'(\w+)="([^"]+)"', stream_str)
+
+        # Converting the list of tuples to a dictionary
+        stream_dict = {key: value for key, value in attributes}
+
+        # Adding the dictionary to the list
+        dict_list.append(stream_dict)
+
+    return dict_list
+
+
+def process_video(video_stream):
+    file_path = os.path.join(VIDEO_FOLDER, video_stream.default_filename)
+    video_stream.download(output_path=VIDEO_FOLDER)
+    return file_path
+
+
+@app.route("/api/download_options", methods=["POST"])
+def download_options():
+    # Clear all files in the directory
+    delete_all_files()
+
+    # Clear the available_resolutions dictionary
+    current_app.available_resolutions.clear()
+
     data = request.get_json()
-    input = data.get("userInput") # defined in your useState
-    word_count = len(input.split(" "))
+    input_link = data.get("inputLink") # defined in useState
+
+    yt_video = YouTube(input_link)
+
+    streams = convert_to_dict_list(yt_video.streams.filter(progressive=True))
+
+    # Create an ordered dictionary to keep the first occurrence of each resolution
+    for stream in streams:
+        if stream["res"] not in current_app.available_resolutions:
+            current_app.available_resolutions[stream["res"]] = stream["itag"]
+    
+    # Convert available_resolutions into the desired list of dictionaries format
+    available_resolutions_list = [{"res": res, "itag": itag} for res, itag in current_app.available_resolutions.items()]
     
     return jsonify({
-        "user_input": f"Your input: {input}",
-        "word_length": f"Input with length {word_count} received!",
+        "available_resolutions": available_resolutions_list,
     })
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
 
+@app.route("/api/download_video", methods=["POST"])
+def download_video():
+    # Clear all files in the directory
+    delete_all_files()
+
+    data = request.get_json()
+    input_resolution = data.get("resolution")
+    saved_link = data.get("savedLink")
+
+    yt_video = YouTube(saved_link)
+
+    # Download the video if the chosen resolution is available
+    if input_resolution in current_app.available_resolutions:
+        video_itag = current_app.available_resolutions[input_resolution]
+        video_file_path = process_video(video_stream=yt_video.streams.get_by_itag(video_itag))
+        video_filename = os.path.basename(video_file_path)
+
+    return jsonify({
+        "message": "Your video is ready to download",
+        "video_url": f"/api/save_video/{video_filename}"
+    })
+
+
+@app.route("/api/save_video/<filename>", methods=["GET"])
+def save_video(filename):
+    return send_from_directory(VIDEO_FOLDER, filename, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(port=8000)
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,40 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useEffect } from "react";
 import { useMutation } from "@tanstack/react-query";
 import Axios from "axios";
+import styles from "@/styles/page.module.scss";
 
 interface InputProps {
-  user_input: string;
-  word_length: string;
+  available_resolutions: {
+    res: string;
+    itag: number;
+  }[];
 }
 
-// Define the mutation function
-const sendUserInput = async (userInput: string) => {
+interface ResolutionProps {
+  message: string;
+  video_url: string;
+}
+
+const sendInputLink = async (inputLink: string) => {
   const response = await Axios.post(
-    "/api/home",
-    { userInput },
+    "/api/download_options",
+    { inputLink },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response.data;
+};
+
+const sendResolution = async (resolution: string, savedLink: string) => {
+  const response = await Axios.post(
+    "/api/download_video",
+    { resolution, savedLink },
     {
       headers: {
         "Content-Type": "application/json",
@@ -25,41 +46,99 @@ const sendUserInput = async (userInput: string) => {
 };
 
 export default function Home() {
-  const [userInput, setUserInput] = useState("");
+  const [inputLink, setInputLink] = useState("");
+  const [savedLink, setSavedLink] = useState("");
+  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
 
-  // Use the useMutation hook
-  const mutation = useMutation<InputProps, Error, typeof userInput>({
-    mutationFn: sendUserInput,
+  const mutation = useMutation<InputProps, Error, typeof inputLink>({
+    mutationFn: sendInputLink,
+  });
+
+  // Mutation for resolution
+  const resolution_mutation = useMutation<
+    ResolutionProps,
+    Error,
+    { resolution: string; savedLink: string }
+  >({
+    mutationFn: ({ resolution, savedLink }) =>
+      sendResolution(resolution, savedLink),
   });
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    mutation.mutate(userInput);
-    setUserInput("");
+    mutation.mutate(inputLink);
+    setSavedLink(inputLink);
+    setInputLink("");
   };
+
+  const chooseResolution = (resolution: string) => {
+    resolution_mutation.mutate({
+      resolution: resolution,
+      savedLink: savedLink,
+    });
+  };
+
+  const clearInputs = () => {
+    setInputLink("");
+    setSavedLink("");
+    mutation.reset();
+    resolution_mutation.reset();
+  };
+
+  useEffect(() => {
+    if (resolution_mutation.isSuccess && resolution_mutation.data?.video_url) {
+      setDownloadUrl(resolution_mutation.data.video_url);
+    }
+  }, [resolution_mutation.isSuccess, resolution_mutation.data]);
 
   return (
     <main>
-      <h1>Type some words</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Enter Link</h1>
+      {savedLink ? null : (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={inputLink}
+            onChange={(e) => setInputLink(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      )}
 
-      <br />
+      {savedLink ? (
+        <button onClick={clearInputs}>Download another video</button>
+      ) : null}
 
       {mutation.isPending && <p>Loading...</p>}
       {mutation.isError && <p>An error occurred.</p>}
-      {mutation.isSuccess && (
-        <div>
-          <p>{mutation.data.user_input}</p>
-          <p>{mutation.data.word_length}</p>
-        </div>
+      {mutation.isSuccess && mutation.data !== undefined && (
+        <ul>
+          {mutation.data.available_resolutions.map((resolution) => (
+            <li key={resolution.itag}>
+              <button
+                onClick={() => chooseResolution(resolution.res)}
+                disabled={resolution_mutation.isPending}
+              >
+                {resolution.res}
+              </button>
+            </li>
+          ))}
+        </ul>
       )}
+
+      {resolution_mutation.isPending && <p>Loading...</p>}
+      {resolution_mutation.isError && <p>An error occurred.</p>}
+      {resolution_mutation.isSuccess &&
+        resolution_mutation.data !== undefined && (
+          <div>
+            <p>{resolution_mutation.data.message}</p>
+            {downloadUrl && (
+              <a href={downloadUrl} download>
+                Click here to download
+              </a>
+            )}
+          </div>
+        )}
     </main>
   );
 }
